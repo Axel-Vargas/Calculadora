ef3de71b55dcd3375376303ca643337d72c19006
Axel Vargas <axelvargasvilla1709@outlook.com>
2024-05-29 19:10:38 -0500

    Prueba 2

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const path = require('path');
-const PhpServer = require('php-server');
-
-// Directorio raíz de tu proyecto PHP
-const root = path.resolve(__dirname);
-
-// Configuración del servidor
-const config = {
-  port: process.env.PORT || 3000, // Puerto en el que se ejecutará el servidor
-  directory: root, // Directorio raíz
-};
-
-// Inicializar el servidor
-const server = new PhpServer(config);
-
-// Iniciar el servidor
-server.run();
+import('php-server').then(PhpServer => {
+    const path = require('path');
+    
+    // Directorio raíz de tu proyecto PHP
+    const root = path.resolve(__dirname);
+    
+    // Configuración del servidor
+    const config = {
+        port: process.env.PORT || 3000, // Puerto en el que se ejecutará el servidor
+        directory: root, // Directorio raíz
+    };
+    
+    // Inicializar el servidor
+    const server = new PhpServer.default(config);
+    
+    // Iniciar el servidor
+    server.run();
+}).catch(err => {
+    console.error('Error al importar el módulo php-server:', err);
+    process.exit(1);
+});
