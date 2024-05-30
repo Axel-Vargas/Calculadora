e67ddb8d14e9e96d80ccfbfdbc9ba10f1e165719
Axel Vargas <axelvargasvilla1709@outlook.com>
2024-05-29 19:22:10 -0500

    Prueba 4

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-// Importar el módulo php-server
-const PhpServer = require('php-server');
-
-// Resto del código sigue igual
-const path = require('path');
-
-// Directorio raíz de tu proyecto PHP
-const root = path.resolve(__dirname);
-
-// Configuración del servidor
-const config = {
-    port: process.env.PORT || 3000, // Puerto en el que se ejecutará el servidor
-    directory: root, // Directorio raíz
-};
-
-// Inicializar el servidor
-const server = new PhpServer(config);
-
-// Iniciar el servidor
-server.run();
+// Utiliza una importación dinámica para cargar el módulo ESM
+import('php-server').then(({ default: PhpServer }) => {
+    const path = require('path');
+    
+    // Directorio raíz de tu proyecto PHP
+    const root = path.resolve(__dirname);
+    
+    // Configuración del servidor
+    const config = {
+        port: process.env.PORT || 3000, // Puerto en el que se ejecutará el servidor
+        directory: root, // Directorio raíz
+    };
+    
+    // Inicializar el servidor
+    const server = new PhpServer(config);
+    
+    // Iniciar el servidor
+    server.run();
+}).catch(err => {
+    console.error('Error al importar el módulo php-server:', err);
+    process.exit(1);
+});
